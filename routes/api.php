aae4138585f1f12492b332de6082e11547609cf2
leoespi <riveraleo113@gmail.com>
2024-10-07 09:56:47 -0500

    agrupacion rutas

--- routes/api.php
+++ routes/api.php
@@ -17,103 +17,62 @@ use App\Http\Controllers\ExcelCesantiasController;
 use App\Http\Controllers\FeedController;
 
 
-Route::put('/updateUser', [UserApiController::class, 'update'])->middleware('auth:api');
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-Route::apiResource('user', UserApiController::class)->middleware('auth:api');
-Route::apiResource('rol', RolApiController::class);
-Route::get('/users', [UserApiController::class, 'index'])->middleware('auth:api');
-
-Route::post('/users/{id}/activate', [UserApiController::class, 'activate'])->middleware('auth:api');
-Route::post('/users/{id}/deactivate', [UserApiController::class, 'deactivate'])->middleware('auth:api');
-
-Route::get('/export-users', [ExcelController::class, 'exportUsers'])->name('export-users')->middleware('auth:api');
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-
-
-
-});
-
-
+// Rutas sin middleware
 Route::post('password/forgot', [UserApiController::class, 'sendResetPin']);
 Route::post('password/reset', [UserApiController::class, 'resetPasswordWithPin']);
-
-
-//EndPoints Incapacidades
-
-Route::apiResource('incapacidades', IncapacidadesController::class)->middleware('auth:api');
-Route::get('incapacidades/{uuid}/downloadFromDB', [IncapacidadesController::class, 'downloadFromDB'])->name('incapacidades.downloadFromDB')->middleware('auth:api');
-Route::get('/export-incapacidades', [ExcelIncapacidadesController::class, 'exportIncapacidades'])->name('export-incapacidades')->middleware('auth:api');
-Route::get('incapacidades/download-zip/{uuid}', [IncapacidadesController::class, 'downloadZip'])->middleware('auth:api');
-
-
-
-
-
-//cesantias
-Route::get('/cesantias/{uuid}/images-size', [CesantiasController::class, 'calculateImagesSizeInMB']); //calcular peso imagenes
-Route::apiResource('cesantias', CesantiasController::class)->middleware('auth:api')->middleware('auth:api');
-Route::get('cesantias/download-zip/{uuid}', [CesantiasController::class, 'downloadZip'])->middleware('auth:api');//Descargar cesantias en formato zip
-Route::get('/export-cesantias/{year}', [ExcelCesantiasController::class, 'exportCesantias'])->name('export-cesantias');//Exportar cesantias excell
-
-//cesantias autorizadas
-Route::put('/cesantias/{id}/authorize', [CesantiasController::class, 'authorizeCesantia'])->middleware('auth:api');//Autorizar cesantias
-Route::get('authorizedCesantia' , [CesantiasController::class,'indexCesantiasAutorizadas' ])->middleware('auth:api');//Ver cesantias autorizadas
-Route::get('authorizedCesantia/download-zip/{uuid}', [CesantiasController::class, 'downloadZipAutorized'])->middleware('auth:api');
-
-//cesantias denegadas
-Route::put('/cesantias/{id}/deny', [CesantiasController::class, 'denyCesantia'])->middleware('auth:api');//denegar cesantias
-
-//Cesantias Denegadas admin 
-Route::post('cesantias/deny/{id}', [CesantiasController::class, 'DenyCesantia'])->middleware('auth:api');
-//Denegar cesantias superadmin
-Route::post('/cesantias/denyadmin/{id}', [CesantiasController::class, 'DenyAuthorizedCesantia'])->middleware('auth:api');
-//cesantias aprobadas
-Route::post('cesantias/aprobar/{id}', [CesantiasController::class, 'AcceptCesantia'])->middleware('auth:api');
-
-
-//Endpoints Referidos
-Route::apiResource('referidos', ReferidosController::class)->middleware('auth:api');
-Route::get('referidos/download/{id}', [ReferidosController::class, 'downloadDocumento'])->middleware('auth:api');
-
-//Endpoints Feed
-//Route::apiResource('feeds', FeedController::class);                                                                                                  ;
-Route::post('feeds', [FeedController::class, 'store'])->middleware('auth:api');
-Route::delete('feeds', [FeedController::class, 'destroy'])->middleware('auth:api');
-Route::get('feeds', [FeedController::class, 'index'])->middleware('auth:api');
-
-
-Route::get('/test', function () {
-    return response([
-        'message' => 'Api is working'
-    ], 200);
-});
-
-
 Route::post('register', [AuthenticationController::class, 'register']);
-
 Route::post('registeradmin', [AuthenticationController::class, 'registerAdmin']);
-
-
 Route::post('login', [AuthenticationController::class, 'login']);
-
-Route::get('/get/user', [UserApiController::class, 'indexUser'])->middleware('auth:api');
-Route::put('/updateUser', [UserApiController::class, 'update'])->middleware('auth:api');
-
-Route::get("/perfil/ver", [PerfilController::class,'verPerfil']);
-
-Route::get('logout', [AuthController::class, "logout"])->middleware('auth:api');
-   
+Route::get('/test', function () {
+    return response(['message' => 'Api is working'], 200);
+});
 Route::get('/Excel', function () {
     return view('Excel');
 });
 
+// Rutas con middleware 'auth:api'
+Route::middleware('auth:api')->group(function () {
+
+    //endpoints Usuarios
+    Route::put('/updateUser', [UserApiController::class, 'update']);
+    Route::apiResource('user', UserApiController::class);
+    Route::get('/users', [UserApiController::class, 'index']);
+    Route::post('/users/{id}/activate', [UserApiController::class, 'activate']);
+    Route::post('/users/{id}/deactivate', [UserApiController::class, 'deactivate']);
+    Route::get('/export-users', [ExcelController::class, 'exportUsers'])->name('export-users');
+    
+    // EndPoints Incapacidades
+    Route::apiResource('incapacidades', IncapacidadesController::class);
+    Route::get('incapacidades/{uuid}/downloadFromDB', [IncapacidadesController::class, 'downloadFromDB'])->name('incapacidades.downloadFromDB');
+    Route::get('/export-incapacidades', [ExcelIncapacidadesController::class, 'exportIncapacidades'])->name('export-incapacidades');
+    Route::get('incapacidades/download-zip/{uuid}', [IncapacidadesController::class, 'downloadZip']);
 
 
+    // EndPoints Cesantias
+    Route::apiResource('cesantias', CesantiasController::class);
+    Route::get('cesantias/download-zip/{uuid}', [CesantiasController::class, 'downloadZip']);
+    Route::get('/export-cesantias/{year}', [ExcelCesantiasController::class, 'exportCesantias'])->name('export-cesantias');
+    Route::put('/cesantias/{id}/authorize', [CesantiasController::class, 'authorizeCesantia']);
+    Route::get('authorizedCesantia', [CesantiasController::class, 'indexCesantiasAutorizadas']);
+    Route::get('authorizedCesantia/download-zip/{uuid}', [CesantiasController::class, 'downloadZipAutorized']);
+    Route::post('cesantias/deny/{id}', [CesantiasController::class, 'DenyCesantia']);
+    Route::post('/cesantias/denyadmin/{id}', [CesantiasController::class, 'DenyAuthorizedCesantia']);
+    Route::post('cesantias/aprobar/{id}', [CesantiasController::class, 'AcceptCesantia']);
 
 
+    Route::get('cesantias/{uuid}/images-size', [CesantiasController::class, 'calculateImagesSizeInMB']);
 
+    
+    // EndPoints Referidos
+    Route::apiResource('referidos', ReferidosController::class);
+    Route::get('referidos/download/{id}', [ReferidosController::class, 'downloadDocumento']);
 
+    // EndPoints Feed
+    Route::apiResource('feeds', FeedController::class);
+    Route::post('feeds', [FeedController::class, 'store']);
+    Route::get('feeds', [FeedController::class, 'index']);
+    
+    Route::get('/get/user', [UserApiController::class, 'indexUser']);
+    Route::get('logout', [AuthController::class, "logout"]);
+    Route::get("/perfil/ver", [PerfilController::class, 'verPerfil']);
+});
